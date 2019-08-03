16bbaccf845621ede33d9f8181443370720019c5
KuangPF <1633397286@qq.com>
2019-08-03 23:44:20 +0800

    chore: add esm format

--- rollup.config.js
+++ rollup.config.js
@@ -3,13 +3,20 @@ import babel from 'rollup-plugin-babel'
 import { terser } from 'rollup-plugin-terser'
 
 const ENV = process.env.NODE_ENV
+
 module.exports = {
   input: 'src/index.ts',
-  output: {
-    name: 'Ribbons',
-    file: 'dist/index.js',
-    format: 'umd'
-  },
+  output: [
+    {
+      name: 'Ribbons',
+      file: 'dist/ribbons.js',
+      format: 'umd'
+    },
+    {
+      file: 'dist/ribbons.esm.js',
+      format: 'esm'
+    }
+  ],
   plugins: [
     typescript(),
     babel({
