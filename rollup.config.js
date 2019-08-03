f79abaf5dc44dcc469920d5f0bb09f76eab7cc13
KuangPF <1633397286@qq.com>
2019-08-03 23:23:17 +0800

    chore: update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -2,18 +2,24 @@ import typescript from 'rollup-plugin-typescript'
 import babel from 'rollup-plugin-babel'
 import { terser } from 'rollup-plugin-terser'
 
+const ENV = process.env.NODE_ENV
 module.exports = {
   input: 'src/index.ts',
   output: {
+    name: 'Ribbons',
     file: 'dist/index.js',
-    format: 'esm'
+    format: 'umd'
   },
   plugins: [
     typescript(),
-    babel(),
-    terser({
-      exclude: ['node_modules/**']
-    })
+    babel({
+      exclude: 'node_modules/**',
+      runtimeHelpers: true
+    }),
+    ENV === 'production' &&
+      terser({
+        exclude: ['node_modules/**']
+      })
   ],
   watch: {
     exclude: ['node_modules/**']
