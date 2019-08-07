0d00907aead4c2e5fb1e8658beb942eb48d7a51a
KuangPF <1633397286@qq.com>
2019-08-07 23:36:10 +0800

    test: add defaultConfig test spec

--- __test__/constructor.spec.ts
+++ __test__/constructor.spec.ts
@@ -1,8 +1,25 @@
-import Ribbons from '../src/index'
+import Ribbons, { isObject } from '../src/index'
+
+afterEach(() => {
+  jest.clearAllMocks()
+})
 
 it('constructor works', () => {
   const ribbonsInstance = new Ribbons({ size: 80, alpha: 0.6, zIndex: 99 })
   expect(ribbonsInstance.config.size).toBe(80)
   expect(ribbonsInstance.config.alpha).toBe(0.6)
   expect(ribbonsInstance.config.zIndex).toBe(99)
+})
+
+it('it will be use defaultConfig', () => {
+  const defaultConfigRibbos = new Ribbons()
+  expect(defaultConfigRibbos.config.size).toBe(90)
+  expect(defaultConfigRibbos.config.alpha).toBe(0.6)
+  expect(defaultConfigRibbos.config.zIndex).toBe(-1)
+})
+
+it('isObject', () => {
+  expect(isObject(null)).toEqual(false)
+  expect(isObject([])).toEqual(true)
+  expect(isObject({})).toEqual(true)
 })
