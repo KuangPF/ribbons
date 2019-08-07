792b9f4a965468fbd03aa925f7eff67cb9d846ac
KuangPF <1633397286@qq.com>
2019-08-07 23:35:20 +0800

    chore: config coverage for jest

--- jest.config.js
+++ jest.config.js
@@ -7,5 +7,9 @@ module.exports = {
     '^.+\\.tsx?$': 'ts-jest'
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  setupFiles: ['./__setups__/canvas.ts', 'jest-canvas-mock']
+  setupFiles: ['./__setups__/canvas.ts', 'jest-canvas-mock'],
+  coverageDirectory: '<rootDir>/__test__/coverage',
+  coveragePathIgnorePatterns: ['/node_modules/', '/test/', '/__tests__/'],
+  coverageReporters: ['json', 'text', 'lcov', 'clover'],
+  collectCoverageFrom: ['src/**/*.{ts,js}']
 }
