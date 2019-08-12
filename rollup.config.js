bcd3dbfa122be221a69df6f3b03621b8d660db0a
KuangPF <1633397286@qq.com>
2019-08-12 22:33:24 +0800

    chore: add banner info

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,28 @@
 import typescript from 'rollup-plugin-typescript'
 import babel from 'rollup-plugin-babel'
 import { terser } from 'rollup-plugin-terser'
+const version = process.env.VERSION || require('./package.json').version
 
+const banner =
+  '/*!\n' +
+  ` * Ribbons.js v${version}\n` +
+  ` * (c) ${new Date().getFullYear()} KuangPF\n` +
+  ' * Released under the MIT License.\n' +
+  ' * @license MIT'
+  ' */'
 const ENV = process.env.NODE_ENV
 
 module.exports = {
   input: 'src/index.ts',
   output: [
     {
+      banner,
       name: 'Ribbons',
       file: 'dist/ribbons.js',
       format: 'umd'
     },
-    {
+    { 
+      banner,
       file: 'dist/ribbons.esm.js',
       format: 'esm'
     }
@@ -25,6 +35,17 @@ module.exports = {
     }),
     ENV === 'production' &&
       terser({
+        output: {
+          comments: function(node, comment) {
+            var text = comment.value;
+            var type = comment.type;
+            console.log(type)
+            if (type == "comment2") {
+              // multiline comment
+              return /@preserve|@license|@cc_on/i.test(text);
+            }
+          }
+        },
         exclude: ['node_modules/**']
       })
   ],
